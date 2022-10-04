3dc947b4a65ca3471dae5698cfb758a9cb872bde
bibitto <bibitto.crypto@gmail.com>
2022-10-04 21:48:10 +0900

    fix: change file name

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from '@mui/material';
+import { theme } from '../theme';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
