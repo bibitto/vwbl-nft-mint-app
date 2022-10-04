79c092bd3d2466db962bca805c4479177b3c52af
bibitto <bibitto.crypto@gmail.com>
2022-10-04 21:50:21 +0900

    create api/verify

---
+++ src/pages/api/verify.ts
@@ -0,0 +1,10 @@
+import { ethers } from 'ethers';
+
+export default async function apiVerify(req, res) {
+  const { message, address: expected, signature } = req.body;
+  const digest = ethers.utils.hashMessage(message);
+  const actual = ethers.utils.recoverAddress(digest, signature);
+  const isVerified = actual === expected;
+
+  res.send({ isVerified });
+}
